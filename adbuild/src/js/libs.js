e22de45b2c204cbc7fade096285fb329cba9da36
DGMary <mblnkinakk@gmail.com>
2019-05-19 21:56:43 +0300

    adbuild was added

---
+++ adbuild/src/js/libs.js
@@ -0,0 +1,6 @@
+//= vendor/jquery-3.3.1.min.js
+//= vendor/formValidate/validate.js
+//= vendor/formstyler/jquery.formstyler.js
+//= vendor/countdown/jquery.countdown.js
+//= vendor/inputMask/jquery.inputmask.js
+//= vendor/masonry/masonry.pkgd.min.js
